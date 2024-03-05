91ebeee1426705731aa68307ff3324207a6fac5a
John N. <120979237+JohnN-Resonat@users.noreply.github.com>
2024-03-05 23:17:10 +0200

    version 0.6.12.35

--- src/app/(pages)/about/sections/SkillsSection/SkillsSection.jsx
+++ src/app/(pages)/about/sections/SkillsSection/SkillsSection.jsx
@@ -27,9 +27,7 @@ export default function SkillsSection() {
                                 <div id="tridiv">
                                     <div className="scene">
                                         <div className="shape cylinder-1 cyl-1">
-                                            <div className="face bm">
-                                                <img src='/icons/sass.svg' />
-                                            </div>
+                                            <div className="face bm" />
                                             <div className="face tp" />
                                             <div className="face side s0" />
                                             <div className="face side s1" />
@@ -40,6 +38,17 @@ export default function SkillsSection() {
                                             <div className="face side s6" />
                                             <div className="face side s7" />
                                             <div className="face side s8" />
+                                            <div className="face side s9" />
+                                            <div className="face side s10" />
+                                            <div className="face side s11" />
+                                            <div className="face side s12" />
+                                            <div className="face side s13" />
+                                            <div className="face side s14" />
+                                            <div className="face side s15" />
+                                            <div className="face side s16" />
+                                            <div className="face side s17" />
+                                            <div className="face side s18" />
+
                                         </div>
                                     </div>
                                 </div>
