24d72cf8e38c3d7fd95ec013bb32119541ddee87
John N. <120979237+JohnN-Resonat@users.noreply.github.com>
2024-03-18 13:15:27 +0200

    version 0.9.4.5

---
+++ src/app/(pages)/contact/components/FormWrapper/FormWrapper.jsx
@@ -0,0 +1,42 @@
+'use client'
+export default function FormWrapper() {
+    function handleSubmit(formEvent) {
+        formEvent.preventDefault();
+
+        const form = document.querySelector('form');
+        const formData = new FormData(form);
+
+        fetch('/', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            body: encodeURI( {'form-name': 'contact', ...formData })
+        })
+        .then(() => console.log('Success!'))
+        .catch(error => console.log(error))
+    }
+
+    return (
+        <div className={'form-wrapper'}>
+            <form onSubmit={handleSubmit} name="contact" method="post" data-netlify="true">
+                <input type="hidden" name="form-name" value="contact" />
+                <div className={'form-input-box'}>
+                    <label htmlFor="fname">Name</label><br />
+                    <input required={true} id="fname" name="contact" type="text" />
+                </div>
+                
+                <div className={'form-input-box'}>
+                    <label htmlFor="femail">Email</label><br />
+                    <input required={true} id="femail" name="contact" type="text" />
+                </div>
+
+                <div className={'form-input-box'}>
+                    <label htmlFor="fmessage">Message</label><br />
+                    <textarea required={true} className="fmessage" id="fmessage" name="contact" type="text" />
+                </div>
+                <div className="submit-button-container">
+                    <button type="submit" className={'start-game-button'}>Submit</button>
+                </div>
+            </form>
+        </div>
+    )
+}
--- src/app/(pages)/contact/page.jsx
+++ src/app/(pages)/contact/page.jsx
@@ -3,6 +3,7 @@ import Footer from "@/app/components/footer/footer";
 import Sidebar from "./components/Sidebar/Sidebar";
 import './styling.scss';
 import CodeWrapper from "./components/CodeWrapper/CodeWrapper";
+import FormWrapper from "./components/FormWrapper/FormWrapper";
 
 
 export default function() {
@@ -18,28 +19,7 @@ export default function() {
                             <h1>// ContactMe</h1>
                         </div>
                         <div className={'form-container'}>
-                            <div className={'form-wrapper'}>
-                                <form name="contact" method="post" data-netlify="true">
-                                    <input type="hidden" name="form-name" value="contact" />
-                                    <div className={'form-input-box'}>
-                                        <label htmlFor="fname">Name</label><br />
-                                        <input required={true} id="fname" name="contact" type="text" />
-                                    </div>
-                                    
-                                    <div className={'form-input-box'}>
-                                        <label htmlFor="femail">Email</label><br />
-                                        <input required={true} id="femail" name="contact" type="text" />
-                                    </div>
-
-                                    <div className={'form-input-box'}>
-                                        <label htmlFor="fmessage">Message</label><br />
-                                        <textarea required={true} className="fmessage" id="fmessage" name="contact" type="text" />
-                                    </div>
-                                    <div className="submit-button-container">
-                                        <button type="submit" className={'start-game-button'}>Submit</button>
-                                    </div>
-                                </form>
-                            </div>
+                        <FormWrapper />
                         <CodeWrapper />
                         </div>
                     </div>
