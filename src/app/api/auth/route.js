1f9d52c17f6730b694fbe28458316cd15ae9cc77
John N. <120979237+JohnN-Resonat@users.noreply.github.com>
2024-04-22 00:54:00 +0300

    version 1.2.12

--- src/app/api/auth/route.js
+++ src/app/api/auth/route.js
@@ -18,25 +18,12 @@ export async function GET(request) {
         redirectUrl.searchParams.set('client_id', client_id);
         redirectUrl.searchParams.set('redirect_uri', response.nextUrl.origin + '/api/callback');
         redirectUrl.searchParams.set('scope', 'repo user');
-        redirectUrl.searchParams.set(
-            'state',
-            crypto.getRandomValues(new Uint8Array(12)).join(''),
-        );
         return Response.redirect(redirectUrl.href, 301);
 
     } catch (error) {
-        
-        const redirectUrl = new URL('https://github.com/login/oauth/authorize');
-        redirectUrl.searchParams.set('client_id', client_id);
-        redirectUrl.searchParams.set('redirect_uri', response.nextUrl.origin + '/api/callback');
-        redirectUrl.searchParams.set('scope', 'repo user');
-        redirectUrl.searchParams.set(
-            'state',
-            crypto.getRandomValues(new Uint8Array(12)).join(''),
-        );
-
-        console.log(redirectUrl.href)
-
-        return new Response(`${redirectUrl.href}, 301`)
+        console.error(error);
+        return new Response(error.message, {
+            status: 500,
+        });
     }
 }
