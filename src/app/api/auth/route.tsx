c007d57d9574b62ff6f2e33e0b76d23ff8d5d4f9
John N. <120979237+JohnN-Resonat@users.noreply.github.com>
2024-04-21 22:43:04 +0300

    version 1.2.11

--- src/app/api/auth/route.tsx
+++ src/app/api/auth/route.tsx
@@ -1,17 +1,17 @@
-export async function onRequest(context) {
+export async function GET(request) {
     const {
-        request, // same as existing Worker API
+        req, // same as existing Worker API
         env, // same as existing Worker API
         params, // if filename includes [id] or [[path]]
         waitUntil, // same as ctx.waitUntil in existing Worker API
         next, // used for middleware or to fetch assets
         data, // arbitrary space for passing data between middlewares
-    } = context;
+    } = await request;
 
-    const client_id = env.GITHUB_CLIENT_ID;
+    const client_id = '0fe9a26f1a94405bb663';
 
     try {
-        const url = new URL(request.url);
+        const url = new URL(req.url);
         const redirectUrl = new URL('https://github.com/login/oauth/authorize');
         redirectUrl.searchParams.set('client_id', client_id);
         redirectUrl.searchParams.set('redirect_uri', url.origin + '/api/callback');
