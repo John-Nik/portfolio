8e0563d6155ddcab4abd65260e832cc9175e7b17
John N. <120979237+JohnN-Resonat@users.noreply.github.com>
2024-05-01 11:13:36 +0300

    version 1.2.21

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -2,14 +2,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-    if (request.nextUrl.pathname === request.nextUrl.pathname.toLowerCase())
-        return NextResponse.next();
-
-    return NextResponse.redirect(new URL(request.nextUrl.origin + request.nextUrl.pathname.toLowerCase()));
-}
-
-export const config = {
-    matcher: "*"
+    return NextResponse.redirect(new URL(request.nextUrl.pathname.toLocaleLowerCase()));
 }
 
 export default middleware;
