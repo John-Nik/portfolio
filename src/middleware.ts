3a6fc3a55a3bc2b9779d07a5bdaecde33744fe3e
John N. <120979237+JohnN-Resonat@users.noreply.github.com>
2024-05-19 11:02:54 +0300

    version 1.3.7

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,17 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-    if (request.nextUrl.pathname != request.nextUrl.pathname.toLowerCase())
+    let lowercaseLink = request.nextUrl.pathname.toLowerCase();
+
+    switch (lowercaseLink) {
+        case '/admin':
+            return NextResponse.rewrite(new URL(request.nextUrl.origin + '/admin/index.html'));
+        case '/dark.css':
+            return NextResponse.redirect(new URL(request.nextUrl.origin + '/admin/dark.css'));
+        case '/config.yml':
+            return NextResponse.redirect(new URL(request.nextUrl.origin + '/admin/config.yml'));
+    }
+
+    if (request.nextUrl.pathname != lowercaseLink)
         return NextResponse.redirect(new URL(request.nextUrl.href.toLowerCase()));
 }
