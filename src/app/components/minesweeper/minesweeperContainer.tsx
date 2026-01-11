fb76a4685928ec63d092cd6dfdb467c3250d760d
John N. <120979237+JohnN-Resonat@users.noreply.github.com>
2026-01-11 17:40:28 +0200

    2.0.0

--- src/app/components/minesweeper/minesweeperContainer.tsx
+++ src/app/components/minesweeper/minesweeperContainer.tsx
@@ -1,7 +1,9 @@
 'use client';
-import Content from './content';
+/* eslint-disable react/jsx-no-comment-textnodes */
 import { useEffect } from 'react';
 import { resetGame, destroyGame } from './gameManager';
+import GameSettings from './components/GameSettings';
+import TextContent from './components/TextContent';
 
 export default function MinesweeperContainer() {
     useEffect(() => {
@@ -17,7 +19,33 @@ export default function MinesweeperContainer() {
             data-minesweeper-container
             className="relative w-full h-full overflow-hidden [&_svg]:pointer-events-none"
         >
-            <Content />
+            <div
+                data-bombs-placed-container
+                className="top-0 left-0 z-10000 fixed flex flex-col opacity-0 w-screen h-screen delay-500 pointer-events-none select-none calm-super-slow"
+            >
+                <div
+                    data-bombs-placed-wrapper
+                    className="top-1/2 left-1/2 relative flex flex-col gap-1 w-fit text-white text-5xl tracking-[2px] -translate-x-1/2 -translate-y-1/2 duration-1200 ease-calm delay-500"
+                >
+                    <span className="leading-none">
+                        // Bombs Placed
+                    </span>
+                    <span
+                        data-bombs-placed-text
+                        className="leading-none"
+                    >
+                        // 465
+                    </span>
+                </div>
+            </div>
+
+            <div className="z-10 absolute flex justify-center items-center w-full h-full pointer-events-none select-none">
+                <div className="relative flex flex-row justify-center lg:justify-between items-center px-4 w-full max-w-[1480px] h-full calm-slow">
+                    <TextContent />
+
+                    <GameSettings />
+                </div>
+            </div>
 
             <div
                 className="relative gap-px grid bg-black w-full h-full overflow-hidden"
